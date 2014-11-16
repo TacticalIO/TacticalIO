d4222134da55b5346a6ca85d1e9acbec1836a8cc
Victor Andritoiu <victor.andritoiu@opentekhnia.com>
2014-11-16 23:08:31 +0100

    switch synchronous

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 exports.TIO = require('./lib/tio').TIO;
-exports.TIOAdapter = require('./lib/tioadapter').TIOAdapter;
+exports.TIOAdapter = require('./lib/adapter').TIOAdapter;
 exports.data = require('./lib/data').data;
 exports.Logger = require('./lib/log').Logger;
 exports.LogApp = require('./lib/log').LogApp;
--- test/test.js
+++ test/test.js
@@ -1,137 +1,41 @@
 #!/usr/local/bin/node
 
 var fs = require('fs'),
-	async = require('async'),
 	debug = require('debug')('TIO-test'),
 	TIO = require('../index').TIO;
 
 var config = JSON.parse(fs.readFileSync('./data/config.json'));
-var tio = new TIO(config, initialized);
+var tio = TIO(config);
 
-function initialized(err) {
-	var self = this;
-	if (!err) {
-		debug('TIO stack initialized [mode = ' + process.env.DEBUG + ']');
-		async.series([
-			function(callback) {
-				self.spiTransfer({
-					data: Buffer("Hello, World!")
-				}, function(err, data) {
-						if (!err) {
-							debug('Result => ' + data);
-						}
-						callback(err);
-				});
-			},
-			function(callback) {
-				self.cs({
-					id: 'FSKCOM-1', 
-					enable: true
-				}, function(err) {
-					if (!err) debug('CS FSKCOM-1 enabled (low level)');
-					callback(err);
-				});
-			},
-			function(callback) {
-				self.cs({
-					id: 'FSKCOM-1', 
-					enable: false
-				}, function(err) {
-					if (!err) debug('CS FSKCOM-1 disabled (low level)');
-					callback(err);
-				});
-			},
-			function(callback) {
-				self.writeDigital({
-					name: 'DO49', 
-					value: true
-				}, function(err) {
-					if (err) {
-						debug('DO49 write failed: ' + err);
-					}
-					callback(err);
-				});
-			},
-			function(callback) {
-				self.readDigital({
-					name: 'DI49' 
-				}, function(err, value) {
-					if (!err) {
-						debug('DI49 = ' + value, ' Test ' + (value ? 'ok' : 'ko'));
-					} else {
-						debug('DI49 read failed: ' + err);
-					}
-					callback(err);
-				});
-			},
-			function(callback) {
-				self.writeDigital({
-					name: 'DO13', 
-					value: true
-				}, function(err) {
-					if (!err) {
-						debug('DO13 write attempt (GPIO64-1), should have used SPI');
-					} else {
-						debug('DO13 write failed: ' + err);
-					}
-					callback(err);
-				});
-			},
-			function(callback) {
-				self.writeGPIO64Digital32({
-					id: 'GPIO64-1',
-					values: '10101010101010101010101010101010'
-				}, function(err) {
-					if (!err) {
-						debug('32 block write attempt (GPIO64-1), should have used SPI');
-					} else {
-						debug('Block write failed: ' + err);
-					}
-					callback(err);
-				});
-			},
-			function(callback) {
-				self.writeGPIO64Digital32({
-					id: 'GPIO64-1',
-					values: [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0]
-				}, function(err) {
-					if (!err) {
-						debug('32 block write attempt (GPIO64-1), should have used SPI');
-					} else {
-						debug('Block write failed: ' + err);
-					}
-					callback(err);
-				});
-			},
-			function(callback) {
-				self.writeGPIO64Digital32({
-					id: 'GPIO64-1',
-					values: 2863311530
-				}, function(err) {
-					if (!err) {
-						debug('32 block write attempt (GPIO64-1), should have used SPI');
-					} else {
-						debug('Block write failed: ' + err);
-					}
-					callback(err);
-				});
-			},
-			function(callback) {
-				self.end(callback);
-			}
-		], function() {
-			debug('All pins unexported');
-			return process.exit(0);
-		});
-	} else {
-		debug(err);
-		process.exit(0);
-	}
+if (tio) {
+	debug('TIO stack initialized [mode = ' + process.env.DEBUG + ']');
+	debug('Result => ' + tio.spiTransfer({ data: Buffer("Hello, World!") }));
+	tio.cs({ id: 'FSKCOM-1', enable: true });
+	tio.cs({ id: 'FSKCOM-1', enable: false });
+	console.time('CS measure');
+	tio.cs({ id: 'FSKCOM-1', enable: true });
+	tio.cs({ id: 'FSKCOM-1', enable: false });
+	console.timeEnd('CS measure');
+	tio.writeDigital({ name: 'DO49', value: true });
+	var ret = tio.readDigital({ name: 'DI49' });
+	debug('DI49 = ' + ret, ' Test ' + (ret ? 'ok' : 'ko'));
+	tio.writeDigital({ name: 'DO13', value: true });
+	debug('DO13 write attempt (GPIO64-1), should have used SPI');
+	elf.writeGPIO64Digital32({
+		id: 'GPIO64-1',
+		values: '10101010101010101010101010101010'
+	});
+	debug('32 block write attempt (GPIO64-1), should have used SPI');
+	tio.writeGPIO64Digital32({
+		id: 'GPIO64-1',
+		values: [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0]
+	});
+	debug('32 block write attempt (GPIO64-1), should have used SPI');
+	tio.writeGPIO64Digital32({ id: 'GPIO64-1', values: 2863311530 });
+	debug('32 block write attempt (GPIO64-1), should have used SPI');
+	tio.end(callback);
+	debug('All pins unexported');
+} else {
+	debug('TIO not initialized');
 }
-
-
-// stop on ctrl-C
-process.on('SIGINT', function() {
-  debug(' [TIO] killed by the user');
-  setTimeout(process.exit, 100, 1);
-});
+process.exit(0);
