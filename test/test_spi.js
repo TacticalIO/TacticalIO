603661a5e508e0a8e30e2f63491b9d8b58f56ae0
Victor Andritoiu <victor.andritoiu@opentekhnia.com>
2014-11-17 12:02:55 +0100

    spi test improvement

--- test/test_spi.js
+++ test/test_spi.js
@@ -10,13 +10,32 @@ var tio = TIO(config);
 if (tio) {
 	debug('TIO stack initialized [mode = ' + process.env.DEBUG + ']');
 	debug('Result => ' + tio.spiTransfer({ data: Buffer("Hello, World!") }));
+	console.time('1000x spi write (13 bytes)');
 	for (var i=0; i<1000; i++) {
-		console.time('spi write');
 		tio.spiTransfer({ data: Buffer("Hello, World!") });
-		console.timeEnd('spi write');
 	}
-	tio.end();
-	debug('All pins unexported');
+	console.timeEnd('1000x spi write (13 bytes)');
+	
+	console.time('1000x spi write (3 bytes)');
+	for (var i=0; i<1000; i++) {
+		tio.spiTransfer({ data: Buffer("Hel") });
+	}
+	console.timeEnd('1000x spi write (3 bytes)');
+
+	console.time('1000x spi write with cs (3 bytes)');
+	for (var i=0; i<1000; i++) {
+		tio.cs( { id: "GPIO64-1", enable: true });
+		tio.spiTransfer({ data: Buffer("Hel") });
+		tio.cs( { id: "GPIO64-1", enable: false });
+	}
+	console.timeEnd('1000x spi write with cs (3 bytes)');
+
+	var timer = setInterval(tio.spiTransfer, 50, { data: Buffer("Hel") });
+
+	setTimeout(function() {
+		tio.end();
+		debug('All pins unexported');
+	}, 3000);
 } else {
 	debug('TIO not initialized');
 }
