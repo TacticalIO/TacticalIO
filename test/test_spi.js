af20a686f6733dd4931abfbb57a8897b49fd0bce
Victor Andritoiu <victor.andritoiu@opentekhnia.com>
2014-11-20 20:27:20 +0100

    spi with integrated cs/csmux

--- test/test_spi.js
+++ test/test_spi.js
@@ -9,6 +9,10 @@ var fs = require('fs'),
 
 var config = JSON.parse(fs.readFileSync('./data/config.json'));
 var tio = TIO(config);
+var buf128 = new Buffer(128);
+buf128.fill(0x13);
+var bufHel = new Buffer(3);
+bufHel.fill(0x31);
 
 if (tio) {
 	debug('TIO stack initialized [mode = ' + process.env.DEBUG + ']');
@@ -21,24 +25,32 @@ if (tio) {
 	
 	console.time('1000x spi write (3 bytes)');
 	for (var i=0; i<1000; i++) {
-		tio.spiTransfer({ data: Buffer("Hel") });
+		tio.spiTransfer({ data: bufHel });
 	}
 	console.timeEnd('1000x spi write (3 bytes)');
 
 	console.time('1000x spi write with cs (3 bytes)');
 	for (var i=0; i<1000; i++) {
 		tio.cs( { id: "GPIO64-1", enable: true });
-		tio.spiTransfer({ data: Buffer("Hel") });
+		tio.spiTransfer({ data: bufHel });
 		tio.cs( { id: "GPIO64-1", enable: false });
 	}
 	console.timeEnd('1000x spi write with cs (3 bytes)');
 
-	var timer = setInterval(tio.spiTransfer, 50, { data: Buffer("Hel") });
+	console.time('1000x spi write with integrated cs (3 bytes)');
+	for (var i=0; i<1000; i++) {
+		tio.spiTransfer({ id: "GPIO64-1", data: bufHel });
+	}
+	console.timeEnd('1000x spi write with integrated cs (3 bytes)');
 
-	setTimeout(function() {
-		tio.end();
-		debug('All pins unexported');
-	}, 3000);
+	console.time('1000x spi write with integrated cs (128 bytes)');
+	for (var i=0; i<1000; i++) {
+		tio.spiTransfer({ id: "GPIO64-1", data: buf128 });
+	}
+	console.timeEnd('1000x spi write with integrated cs (128 bytes)');
+
+	tio.end();
+	debug('All pins unexported');
 } else {
 	debug('TIO not initialized');
 }
