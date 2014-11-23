2ae0611e96490671cdfb97c3429f4fd23140a2e4
Victor Andritoiu <victor.andritoiu@opentekhnia.com>
2014-11-23 18:23:34 +0100

    integrate aio functions

---
+++ test/test_aio.js
@@ -0,0 +1,61 @@
+#!/usr/local/bin/node
+
+var fs = require('fs'),
+	debug = require('debug')('TIO-test'),
+	TIO = require('../index').TIO;
+
+var config = JSON.parse(fs.readFileSync('./data/config.json'));
+var tio = TIO(config);
+var buf128 = new Buffer(128);
+buf128.fill(0x13);
+var bufHel = new Buffer(3);
+bufHel.fill(0x31);
+
+if (tio) {
+	debug('TIO stack initialized [mode = ' + process.env.DEBUG + ']');
+	console.time('TIO stack hardware reset');
+	tio.hwReset();
+	console.timeEnd('TIO stack hardware reset');
+
+	console.time('TIO stack sync clock hardware reset');
+	tio.hwSyncReset();
+	console.timeEnd('TIO stack sync clock hardware reset');
+
+	console.time('clock reset');
+	tio.resetClock();
+	console.timeEnd('clock reset');
+
+	console.time('board id');
+	var aio_id = tio.iid( { id: 'AIO12-1'});
+	console.timeEnd('board id');	
+	console.log('AIO12-1 FPGA id: ', aio_id);
+
+	console.time('1000x 5V sin ana out');
+	for (var i=0; i<1000; i++) {
+		tio.writeAnalog({ name: 'AO00', value: 5*Math.sin(i*2*Math.PI), when: 0 });
+	}
+	console.timeEnd('1000x 5V sin ana out');
+
+	console.time('1000x 5V sin all ana out');
+	for (var i=0; i<1000; i++) {
+		tio.writeAnalog({ name: 'AO00', value: 5*Math.sin(i*2*Math.PI), when: 0 });
+		tio.writeAnalog({ name: 'AO01', value: 5*Math.sin(i*2*Math.PI), when: 0 });
+		tio.writeAnalog({ name: 'AO02', value: 5*Math.sin(i*2*Math.PI), when: 0 });
+		tio.writeAnalog({ name: 'AO03', value: 5*Math.sin(i*2*Math.PI), when: 0 });
+	}
+	console.timeEnd('1000x 5V sin all ana out');
+
+	console.time('get clock');
+	var clock = tio.getClock({ id : 'AIO12-1' });
+	console.timeEnd('get clock');
+
+	tio.writeAnalog({ name: 'AO00', value: 5*Math.sin(i*2*Math.PI), when: clock + 1500 });
+
+	setTimeout(function() {
+		tio.end();
+		debug('All pins unexported');
+	}, 1600*1.024);
+} else {
+	debug('TIO not initialized');
+}
+process.exit(0);
