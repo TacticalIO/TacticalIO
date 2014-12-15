e608fc9eb956e93fd778d7b6134f8337264d7d77
Victor Andritoiu <victor.andritoiu@opentekhnia.com>
2014-12-15 20:44:11 +0100

    remoting: Remoter

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 exports.TIO = require('./lib/tio').TIO;
 exports.TIOAdapter = require('./lib/adapter').TIOAdapter;
+exports.TIORemoter = require('./lib/adapter').TIORemoter;
 exports.data = require('./lib/data').data;
 exports.Logger = require('./lib/log').Logger;
 exports.LogApp = require('./lib/log').LogApp;
